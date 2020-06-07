c62cd3e3308d23dfe9edd497384f03ee88c282a9
Larissa Santos Azevedo <larissa.santosazevedo@hotmail.com>
2020-06-07 20:18:44 -0300

    finish backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 
+import PointController from './controllers/PointController';
+import ItemsController from './controllers/ItemsController';
+
 const routes = express.Router();
+const pointController = new PointController();
+const itemsController = new ItemsController();
 
+routes.get('/items', itemsController.index );
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello' });
-});
+routes.post('/points', pointController.create);
+routes.get('/points', pointController.index);
+routes.get('/points/:id', pointController.show);
 
 export default routes;
