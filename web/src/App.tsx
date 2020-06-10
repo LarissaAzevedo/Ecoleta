eb5b615ffa403db8c22012bd30475cd72b363408
Larissa Santos Azevedo <larissa.santosazevedo@hotmail.com>
2020-06-10 02:31:56 -0300

    started web

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint';
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <CreatePoint />
+    </div>
+  );
+}
+
+export default App;
